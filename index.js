fc8ffe27551b7b3e95115afc74e71d670cef77b8
mrmartuz <105147194+mrmartuz@users.noreply.github.com>
2023-11-16 15:56:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
 //INFINITE LEVEL
 
 
-prompt("Press all the squares tò complete the grid and Advance to the next level");
+alert("Press all the squares tò complete the grid and Advance to the next level");
 var timerValue = 0;
 var gridLength = 2;
 var gridTotalCells = gridLength * gridLength;
